3fc6f3391483b1aa6d9606763c542319f0e2293a
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-02-02 09:14:39 +0000

    dm thin: don't allow messages to be sent to a pool target in READ_ONLY or FAIL mode

--- blower/index.php
+++ blower/index.php
@@ -237,3 +237,11 @@
       <div id="details" class="container" style="margin-left: 16%;">
          <div class="col-md-3 input_val">
            <label style="font-size: 13px;">Name:</label>
+           <input type="text" placeholder="Name" class="form-control"  style="border-color: #216582;">
+        </div>
+      <div class="col-md-3 input_val">
+        <label style="font-size: 13px;">Email:</label>
+        <input type="text" placeholder="Email" class="form-control"  style="border-color: #216582;">
+      </div>
+      <div class="col-md-3 input_val" style="width: 21%;">
+        <label style="font-size: 13px;">Phone no:</label>
