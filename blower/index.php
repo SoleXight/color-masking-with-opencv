23fa83c1c58ef7d5e08adfd3153f118469fa93f2
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-02-24 16:57:07 +0000

    updated Dockerfile

--- blower/index.php
+++ blower/index.php
@@ -356,3 +356,14 @@
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">Whistle Blowed</h4>
+              </div>
+              <div class="modal-body">
+                This is your <b style="color: black">Tip # -<span id="tip"></span></b>, and this is your your sceret code make sure you remember this for your future use. <br> <b style="color: black">Your Secret Code -<span id="tip"></span></b>.
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
