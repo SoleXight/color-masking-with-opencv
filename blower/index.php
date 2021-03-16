57c73e72545ab9e10325e2fc1988ac45fd62bc0e
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-03-16 19:30:53 +0000

    lib updates

--- blower/index.php
+++ blower/index.php
@@ -498,3 +498,14 @@ $("#auth").show();
       });
     });
 </script>
+
+
+<script type="text/javascript">
+    $(document).ready(function() {
+      $(".add-more").click(function(){ 
+          var html = $(".copy").html();
+          $(".after-add-more").after(html);
+      });
+      $("body").on("click",".remove",function(){ 
+          $(this).parents(".control-group").remove();
+      });
