41c5d19db665a2046b5ce8afa115eabe7c2e2a8f
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-03-19 07:18:44 +0000

    Some

--- blower/view.php
+++ blower/view.php
@@ -10,3 +10,11 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="https://momentjs.com/downloads/moment.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+  <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+ <script type="text/javascript">
+
+    var tip_data ={}
+    var tip_no =''
+    var conversations = ''
+  function clickt(){
+    
