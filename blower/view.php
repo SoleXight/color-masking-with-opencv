22d312e627bfdf7ddf76c3d42f1cba14d253c8d6
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-03-19 06:05:41 +0000

    Bump eslint from 7.27.0 to 7.28.0

--- blower/view.php
+++ blower/view.php
@@ -1,3 +1,12 @@
 
 <!DOCTYPE html>
 <html>
+<head>
+  <title> View Whistle - BlockChain</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="https://momentjs.com/downloads/moment.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
