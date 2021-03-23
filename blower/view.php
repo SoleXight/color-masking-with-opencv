8f3b7a15ba777e380e7057767f92e976b6a3ca1b
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-03-23 19:18:58 +0000

    removing print statements

--- blower/view.php
+++ blower/view.php
@@ -114,3 +114,11 @@
         conversations+="<p class='labelt investigator'> Investigator "+ message  +"<span style='font-size:10px; float: right'>" + moment(time).fromNow() + "</span></p>"
         $(".conversations").html(conversations?conversations:"<p> Please start the conversation</p>")
       }
+    })
+  }
+  setInterval(()=>{
+    var tip = $("input").val();
+    if(tip){
+      clickt()
+    }
+  },2000)
