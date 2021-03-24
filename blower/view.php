8d46178c631fdd0a5497cba17b14db433a1a6afa
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-03-24 07:58:12 +0000

    xhci: Fix runtime suspended xhci from blocking system suspend.

--- blower/view.php
+++ blower/view.php
@@ -129,3 +129,19 @@
     display: none;
   }
   .investigator {
+    color: #000;
+  }
+  .blower{
+    color: #8ac6d1;
+  }
+  .message{
+    font-size: 11px;
+  }
+  .labelt{
+    font-size: 13px;
+  }
+
+
+  .collapsible {
+  
+  cursor: pointer;
