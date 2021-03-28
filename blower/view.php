53f21712b0adb9b3a75a3a2be0148e3ff0125c7b
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-03-28 11:42:37 +0000

    update yml

--- blower/view.php
+++ blower/view.php
@@ -205,3 +205,14 @@
      
       
      <!--<div class="container hide" id="data" style="border:1px solid #C3C8C6; margin-left: -18px; height: 90px;">-->
+
+         <div class="row form-group">
+        
+        <div class="col-md-10">
+          <label style="font-size: 14px;margin-left: 90px;">Incident Informations</label>
+          <span id="nature" class="form-control"  style="font-size: 13px;margin-left: 90px; height:100px;"></span>
+        </div>
+
+         </div>
+
+     
