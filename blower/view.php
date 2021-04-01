234355858672d21b017d788420095e1b472c3a80
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-04-01 18:42:37 +0000

    Update the Renderer API.

--- blower/view.php
+++ blower/view.php
@@ -258,3 +258,16 @@
    <!--<div class="container hide" id="data" style="border:1px solid #C3C8C6; margin-left: -18px; height: 90px;">-->
       <div class="row form-group">
               
+
+
+        <!--<div class="col-md-5">
+          <label style="font-size: 14px;margin-left: 90px;">Persons Involved</label><br>
+          <span id="personsInvolved" class="form-control" style="font-size: 13px; margin-left: 90px;"></span>
+        </div>-->
+        <div class="col-md-5">
+          <label style="font-size: 14px;margin-left: 90px;">Authorities Know</label><br>
+          <span id="authknows" class="form-control" style="font-size: 13px;margin-left: 90px;"></span>
+        </div>
+
+        <div class="col-md-5">
+          <label style="font-size: 14px;margin-left: 90px;">Reward</label><br>
