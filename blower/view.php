84e1c2f3e2c01abc3a125a43ed1be74b5549ebc0
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-04-05 23:08:55 +0000

    Drop svn:executable bit from po files

--- blower/view.php
+++ blower/view.php
@@ -393,3 +393,14 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+</script>
+<!-- <script>
+$(document).ready(function(){
+  $("#authknowsarea").hide();
