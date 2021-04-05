744b1090fc50fdccf509bdd74d22e93bde7be3cc
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-04-05 18:37:08 +0000

    Added exception handling.

--- blower/view.php
+++ blower/view.php
@@ -370,3 +370,14 @@
 
 
       $(".add-more").click(function(){ 
+          var html = $(".copy").html();
+          $(".after-add-more").after(html);
+      });
+
+
+      $("body").on("click",".remove",function(){ 
+          $(this).parents(".control-group").remove();
+      });
+
+
+    });
