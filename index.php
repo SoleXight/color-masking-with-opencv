7d702e7257a1a38e0313043cb0fa474ad24207e1
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-04-06 19:59:10 +0000

    modified title

--- index.php
+++ index.php
@@ -9,3 +9,16 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>WB- V6.0</title>
+
+    <script type="text/javascript">
+     
+         setTimeout(function(){
+            window.location.href = './blower/index.php';
+         }, 6000);
+      
