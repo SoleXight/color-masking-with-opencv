4ae7fee58ac73257ad17c20dd297dca53c098ec9
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-04-13 07:56:02 +0000

    init docs

--- index.php
+++ index.php
@@ -135,3 +135,14 @@ a{
    <h6 style="position: fixed; margin-top: 170px; font-weight: bolder; font-size:26px;">Whistle Blower</h6>
 
   <h6 style="position: fixed; margin-top: 200px; font-weight: bolder; font-size: 21px; color: #690B3D">Powered By Corda</h6>
+
+</div>
+
+</center>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
