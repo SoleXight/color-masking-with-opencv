f492c7cb15bda21575a89926bf8390dc66a7798d
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-04-20 14:51:04 +0000

    fix tooltip

--- investigator/dashboard.php
+++ investigator/dashboard.php
@@ -115,3 +115,14 @@ Highcharts.chart('container', {
         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
     },
     plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+            }
+        }
+    },
+    series: [{
+        name: 'company',
