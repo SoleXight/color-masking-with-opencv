0cfb4df317ddbf6bd60abd1de61d7ff527d35f24
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-04-25 10:14:05 +0000

    added cors

--- investigator/header.php
+++ investigator/header.php
@@ -2,4 +2,5 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-sca
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <
