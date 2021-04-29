78081ec9f3f05cb3631a3b775ba311c0e4ee8561
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-04-29 16:03:17 +0000

    doc ja: translate po

--- investigator/header.php
+++ investigator/header.php
@@ -3,4 +3,4 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.
