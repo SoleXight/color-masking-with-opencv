8490a6b9ef74484cfdf2a8e716b83c22cc832735
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-04-29 20:02:01 +0000

    setup travis

--- investigator/header.php
+++ investigator/header.php
@@ -3,4 +3,5 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+ 
