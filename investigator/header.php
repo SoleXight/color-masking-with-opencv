2b4a76e0b3866a6dcba6caef936ed61c2faa3aba
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-04-30 16:58:08 +0000

    Add LICENSE.md

--- investigator/header.php
+++ investigator/header.php
@@ -4,4 +4,4 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.g
+  <script src="https://ajax.googleapis.com/aj
