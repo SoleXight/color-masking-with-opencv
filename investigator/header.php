a6b7ff8039b9c7242eeaeea73293904756ea956c
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-05-05 14:46:43 +0000

    new blog stuff

--- investigator/header.php
+++ investigator/header.php
@@ -6,4 +6,4 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <link href="https://fonts.googleap
+  <link href="https://fonts.googleapis.com
