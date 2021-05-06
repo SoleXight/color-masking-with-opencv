939fa46159dbabe32b45218b821e97f7ffde4b38
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-05-06 22:39:44 +0000

    Fixes build.

--- investigator/header.php
+++ investigator/header.php
@@ -7,3 +7,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+<na
