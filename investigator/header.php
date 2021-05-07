04adbf85a8afb262c6e80c39105482d1c09bf539
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-05-07 20:42:28 +0000

    Fix PEP8

--- investigator/header.php
+++ investigator/header.php
@@ -9,4 +9,4 @@
   <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-invers
+<nav class="navbar navbar-inverse navba
