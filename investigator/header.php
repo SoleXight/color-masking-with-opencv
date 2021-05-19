5d6397cefbf2681e5718582990e49c36d92ab3b4
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-05-19 06:38:51 +0000

    update text color

--- investigator/header.php
+++ investigator/header.php
@@ -11,4 +11,5 @@
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top" style="background: #af0404; height: 70px; border:none; ">
   <div class="container">
-    <div class="navbar-header
+    <div class="navbar-header">
+   
