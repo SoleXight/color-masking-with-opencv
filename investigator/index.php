1c7e4f107abfb9040fdabcdac948b8a00d375afa
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-08-08 13:55:23 +0000

    Update FrontEnd.md

--- investigator/index.php
+++ investigator/index.php
@@ -1,2 +1,8 @@
 
 <!DOCTYPE html>
+<html>
+<head>
+	<title>Investigator - BlockChain</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
