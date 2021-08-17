abf92de19b5a518ef5aa25fd2096471cda473549
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-08-17 06:37:59 +0000

    mm, thp: close race between mremap() and split_huge_page()

--- investigator/index.php
+++ investigator/index.php
@@ -142,3 +142,10 @@
 <?php include "header.php"; ?>
 
 <div style="margin-left: 140px;">
+<div class="container" style="margin-top: 150px;margin-left: 35%;">
+	
+	<div class="form-group" style="margin-left: -10px;">
+		<div class="col-md-3">
+			<input type="text" name="search" id="search" class="form-control col-md-3" placeholder="Enter the Tip Number">
+			
+		</div>
