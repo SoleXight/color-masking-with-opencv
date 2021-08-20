5d6336ea33e8d706b25131091a32a10acd7a194a
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-08-20 17:47:31 +0000

    standards

--- investigator/index.php
+++ investigator/index.php
@@ -232,3 +232,7 @@
 
  <!--<div class="container hide" id="data" style="border:1px solid #C3C8C6; margin-left: -18px; height: 90px;">-->
 		<div class="row form-group">
+            
+
+
+			<!--<div class="col-md-5">
