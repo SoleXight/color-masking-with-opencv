1c750084330e3b683ed403fa6d5bdc5da7e398f4
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-09-25 08:02:52 +0000

    sp.

--- investigator/index.php
+++ investigator/index.php
@@ -523,3 +523,14 @@
 			</div>
 
 					</div><br><br>
+			</div>
+
+			    <div class="modal fade" id="myModal" role="dialog">
+			    <div class="modal-dialog">
+			    
+			      <!-- Modal content-->
+			      <div class="modal-content">
+			        <div class="modal-header">
+			          <button type="button" class="close" data-dismiss="modal" >&times;</button>
+			          <h4 class="modal-title">Blower - Investigator's Conversation's</h4>
+			        </div>
