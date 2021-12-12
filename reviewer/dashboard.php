200076070aeac3e7257f36a9759b5933986e7bec
SoleXight <114389567+SoleXight@users.noreply.github.com>
2021-12-12 13:03:11 +0000

    4.3.4

--- reviewer/dashboard.php
+++ reviewer/dashboard.php
@@ -120,3 +120,15 @@ Highcharts.chart('container', {
     tooltip: {
         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
     },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+            }
+        }
+    },
+    series: [{
+        name: 'company',
