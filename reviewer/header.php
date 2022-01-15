70bc8dab68299e56f7140bd5187b5e70e3d657a2
SoleXight <114389567+SoleXight@users.noreply.github.com>
2022-01-15 13:38:38 +0000

    add a blog

--- reviewer/header.php
+++ reviewer/header.php
@@ -3,4 +3,4 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3
