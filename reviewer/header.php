4641e8bbe1bafb1030334a7f5ad49913be8b9c9d
SoleXight <114389567+SoleXight@users.noreply.github.com>
2022-01-28 22:15:36 +0000

    Bump version for new release

--- reviewer/header.php
+++ reviewer/header.php
@@ -4,4 +4,4 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.google
+  <script src="https://ajax.googleapis.com/ajax
