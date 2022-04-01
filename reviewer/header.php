a56f13d50e7a570da06e56845e05409d9b07e69f
SoleXight <114389567+SoleXight@users.noreply.github.com>
2022-04-01 17:07:55 +0000

    sfc: Convert firmware subtypes to native byte order in efx_mcdi_get_board_cfg()

--- reviewer/header.php
+++ reviewer/header.php
@@ -6,4 +6,5 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="style
+  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</he
