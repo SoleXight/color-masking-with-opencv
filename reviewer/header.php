2b93525f0b7c954384c3ea483d5a142e9eac83a4
SoleXight <114389567+SoleXight@users.noreply.github.com>
2022-04-10 17:56:13 +0000

    Fixes #131

--- reviewer/header.php
+++ reviewer/header.php
@@ -9,4 +9,4 @@
   <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-inverse navbar-fixed
+<nav class="navbar navbar-inverse navbar-fixed-top" s
