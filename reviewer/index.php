a2c5c77efd4c8d1d6d7f51f3884e3ff71809f629
SoleXight <114389567+SoleXight@users.noreply.github.com>
2022-12-28 16:32:47 +0000

    Terminology

--- reviewer/index.php
+++ reviewer/index.php
@@ -387,3 +387,11 @@
 			    </div>
 			    <div class="col-md-4 input_val">
 			      <label style="font-size: 13px;">Designation:</label>
+			      <input type="text" placeholder="Designation" class="form-control" style="border-color: #216582;">
+			    </div>
+			    <div class="col-md-3 input_val" style="width: 29%;">
+			      <label style="font-size: 13px;">Department:</label>
+			      <input type="text" placeholder="Department" class="form-control" style="border-color: #216582;">
+			    </div>
+			              <button class="btn btn-danger remove" type="button" style="margin-top: 24px;margin-left: 60px;"><i class="glyphicon glyphicon-remove"></i></button>
+			          </div>
