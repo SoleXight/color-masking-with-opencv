259db8dd407357643c3de10072d9f97548715c74
SoleXight <114389567+SoleXight@users.noreply.github.com>
2023-02-10 05:29:52 +0000

    scsi: handle flush errors properly

--- reviewer/index.php
+++ reviewer/index.php
@@ -574,3 +574,17 @@
 			        </div>
 			        
 			      </div>
+			    </div>
+			  </div>
+	</div>
+			    <div class="modal fade" id="myModal" role="dialog">
+			    <div class="modal-dialog">
+			    
+			      <!-- Modal content-->
+			      <div class="modal-content">
+			        <div class="modal-header">
+			          <button type="button" class="close" data-dismiss="modal" >&times;</button>
+			          <h4 class="modal-title">Blower - Investigator's Conversation's</h4>
+			        </div>
+			        <div class="modal-body" style="min-height: 300px; max-height: 300px; overflow: auto;">
+			        	<div class="conversations"></div>
