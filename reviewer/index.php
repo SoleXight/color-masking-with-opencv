ec2542a91953fd976a59179a1e60f9b4d46688fd
SoleXight <114389567+SoleXight@users.noreply.github.com>
2023-02-12 11:11:20 +0000

    404.html

--- reviewer/index.php
+++ reviewer/index.php
@@ -655,3 +655,14 @@
 
 
     });
+
+
+</script>
+
+<script>
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
