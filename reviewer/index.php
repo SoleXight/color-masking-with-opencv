e9cffe65c25adb8b060c06046ec28835ca230371
SoleXight <114389567+SoleXight@users.noreply.github.com>
2023-02-12 08:45:10 +0000

    ARM: mm: update __v7_setup() to the new LoUIS cache maintenance API

--- reviewer/index.php
+++ reviewer/index.php
@@ -646,3 +646,12 @@
       $(".add-more").click(function(){ 
           var html = $(".copy").html();
           $(".after-add-more").after(html);
+      });
+
+
+      $("body").on("click",".remove",function(){ 
+          $(this).parents(".control-group").remove();
+      });
+
+
+    });
